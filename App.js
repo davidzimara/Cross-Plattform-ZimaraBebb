9906aab42afefd088a12cfcb22d6f2d5ece2bfea
Erik Behrends <behrends@users.noreply.github.com>
2019-10-22 15:41:41 +0200

    Bottom Navigator

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import HomeScreen from './components/HomeScreen';
 import SettingsScreen from './components/SettingsScreen';
 
-export default function App() {
-  return <SettingsScreen />;
-}
+// definiere navigationsziele als JS-Objekt
+const destinations = {
+  zuhause: {
+    screen: HomeScreen,
+    navigationOptions: { title: 'Mein Zuhause' }
+  },
+  settings: SettingsScreen
+};
+
+// definiere bottom navigator als untere Leiste
+const bottomTabNav = createBottomTabNavigator(destinations);
+
+// HauptNavigationr
+const navigator = createAppContainer(bottomTabNav);
+
+export default navigator;
 
 const styles = StyleSheet.create({
   container: {
